18570116e3137090782d6c57aee1fdebfd3fcdde
ottomayer313 <ottomayer313@gmail.com>
2023-02-21 08:34:49 -0800

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,36 @@
+[![Current Version](https://img.shields.io/badge/version-0.0.1-green.svg)](https://github.com/OttoMayer313/chatbot-zenkaruto)
+
+# Build
+Open the casino.html in browser.
+
+the index.php file is just used for hosting on heroku
+
+
+# Casino
+Roulette Table Simulator for Red/Black, High/Low, anything with the ~48% probability to test the  strategies eg. Martingale Strategy
+
+Simple simulator over 1,10 or 100 bets.
+
+# Navigation
+* click the roulette table or press enter to spin
+* click the bag of money add money to wallet
+* hover over on win/on lose to change behaviour on win/lose
+* click reset to reset
+
+# Default Variables
+**found at the top of the casino.js file
+- MAX_BET - max bet amount per spin. Default:400;
+- MIN_BET - minimum bet amount. Default:0;
+- CASH_ADD - how much more cash to "top up" wallet when you need to. Default:100;
+- CASH_DEFAULT - how much cash you start with. Default:300;
+
+# ON WIN/LOSS
+how you will change your bet each win and or loss per spin.
+
+### Examples:
+- ONWIN = 0.5; when you bet 10 and win, the next bet will be 5
+- ONLOSE = 2; when you bet 10 and lose, the next bet will be 20
+- ONWIN = 1; when you bet 10 and win, the next bet will be 10
+
+# Rand
+it uses system rand
---
+++ casino.js
@@ -0,0 +1,135 @@
+/*
+Otto Mayer
+Roulette Simulator
+*/
+
+var MAX_BET = 400;
+var MIN_BET = 0;
+var CASH_ADD = 100; //how much cash to top by up when needed
+var CASH_DEFAULT = 300; //default cash to start with
+
+
+//get objects
+var spin = document.querySelector("#spin");
+var win = document.querySelector("#win");
+var lose = document.querySelector("#lose");
+var bet = document.querySelector('#betAmount');
+var cash = document.querySelector("#cash");
+cash.innerHTML = CASH_DEFAULT;
+
+//events
+document.getElementById("moneybag").addEventListener("click",addMoney);
+document.getElementById("panelWheel").addEventListener("click",calculateSpin);
+document.getElementById("resetButton").addEventListener("click",resetAll);
+bet.addEventListener("focus",function(){this.value=""});
+
+//Submit
+$( "#formOptions" ).submit(function( event ) {
+    event.preventDefault();
+    if(verifyCustomInput(bet)){
+        calculateSpin();
+    }
+});
+
+//Roulette random outcome for Red/Black
+function rouletteBlackRed() {
+    return Math.floor(Math.random() * 100)<47;
+}
+
+
+function calculateSpin() {
+    var onwin_i = parseFloat(document.querySelector('input[name="onWinButton"]:checked').value);
+    var onlose_i = parseFloat(document.querySelector('input[name="onLoseButton"]:checked').value);
+    var loop_i = parseInt(document.querySelector('input[name="loopSelector"]:checked').value);
+    var spin_i = parseInt(spin.innerHTML);
+    var win_i = parseInt(win.innerHTML);
+    var lose_i = parseInt(lose.innerHTML);
+    var bet_i = parseInt(bet.value);
+    var cash_i = parseInt(cash.innerHTML);
+
+    var stoploop_i = false;
+
+    if(bet_i>MAX_BET){
+        bet_i = MAX_BET;
+        bet.value = MAX_BET;
+    }
+    if (bet_i<MIN_BET){
+        bet_i = MIN_BET;
+        bet.value = MIN_BET;
+    }
+
+    //calculate a single spin, update spin win loss cash
+    function singleSpin() {
+        if((bet_i>cash_i) || (bet_i===0)){
+            bet.className="error";
+            stoploop_i = true;
+            return stoploop_i;
+        }
+
+        spin_i++;
+        if (rouletteBlackRed()){
+            cash_i+=bet_i;
+            win_i++;
+
+            if(onwin_i===0) {
+                stoploop_i = true;
+            }
+            else {
+                bet_i = Math.floor(bet_i * onwin_i);
+            }
+        }
+        else {
+            cash_i-=bet_i;
+            lose_i++;
+
+            if(onlose_i===0) {
+                stoploop_i = true;
+            }
+            else {
+                bet_i = Math.floor(bet_i * onlose_i);
+            }
+        }
+        return stoploop_i;
+    }
+
+    for (var i=0;i<loop_i;i++) {
+        if(singleSpin()){
+            break;
+        }
+    }
+
+    //update global vars
+    spin.innerHTML = spin_i;
+    win.innerHTML = win_i;
+    lose.innerHTML = lose_i;
+    bet.value = bet_i;
+    cash.innerHTML = cash_i;
+}
+
+// -- functions
+
+function resetAll() {
+    cash.innerHTML = CASH_DEFAULT;
+    spin.innerHTML = 0;
+    win.innerHTML = 0;
+    lose.innerHTML = 0;
+    //todo: prob better way of setting view and value, so dont have to set twice?
+}
+
+//incrementally add money to purse
+function addMoney() {
+    cash.innerHTML = parseInt(cash.innerHTML) + CASH_ADD;
+}
+
+//input validation: numbers only
+function verifyCustomInput(input) {
+    if (/^[0-9]*$/.test(""+input.value) && (input.value!=="")){
+        input.className = "valid";
+        return true;
+    }
+    else {
+        input.className = "error";
+        input.value = "";
+        return false;
+    }
+}
